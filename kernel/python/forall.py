3b070e4b4aa76471c53d8c807f066306688bf872
Tiago Tavares <tiagoft@gmail.com>
2014-11-19 14:06:34 -0200

    [fix_deploy] Using basic example

--- kernel/python/forall.py
+++ kernel/python/forall.py
@@ -1,12 +1,26 @@
 
+
 import web
-import forall.webapp
 
-
-urls = ("/", "forall.webapp.WebApp")
-
+urls = ("/.*", "hello")
 app = web.application(urls, globals())
 
+class hello:
+    def GET(self):
+        return 'Hello, world!'
 
 if __name__ == "__main__":
     app.run()
+
+
+#import web
+#import forall.webapp
+
+
+#urls = ("/", "forall.webapp.WebApp")
+
+#app = web.application(urls, globals())
+
+
+#if __name__ == "__main__":
+#    app.run()
