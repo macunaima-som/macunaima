cfe2e7f6a63966fe6db7d51f6a94fe34cf29a25b
Tiago Tavares <tiagoft@gmail.com>
2014-11-19 14:15:44 -0200

    [fix_deploy] Tried to use our system

--- kernel/python/forall.py
+++ kernel/python/forall.py
@@ -1,24 +1,10 @@
 
 
 import web
+import forall.webapp
 
-urls = ("/.*", "hello")
+
+urls = ("/", "forall.webapp.WebApp")
 application = web.application(urls, globals()).wsgifunc()
 
-class hello:
-    def GET(self):
-        return 'Hello, world!'
 
-
-
-#import web
-#import forall.webapp
-
-
-#urls = ("/", "forall.webapp.WebApp")
-
-#app = web.application(urls, globals())
-
-
-#if __name__ == "__main__":
-#    app.run()
